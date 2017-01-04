3860da209ba4a5188e84ff54a0611ff0a08f6538
AndreiBarsan <andrei.ioan.barsan@gmail.com>
2017-01-04 20:59:01 +0200

    Started some cleanup.

--- PythonEvaluationTools/vqaEvalDemo.py
+++ PythonEvaluationTools/vqaEvalDemo.py
@@ -12,7 +12,10 @@ matplotlib.use('Agg')
 # data_dir = '../../VQA'
 data_dir = '/data/vqa'
 code_dir = '.'
-experiment_dir = '/tmp'
+
+# TODO(andrei): Better argument processing.
+experiment_dir = sys.argv[1]
+print("Using experiment dir: [{0}]".format(experiment_dir))
 
 sys.path.insert(0, '%s/PythonHelperTools/vqaTools' % (code_dir))
 
@@ -32,15 +35,16 @@ dataSubType ='val2014'
 annFile     ='%s/Annotations/%s_%s_annotations.json'%(data_dir, dataType, dataSubType)
 quesFile    ='%s/Questions/%s_%s_%s_questions.json'%(data_dir, taskType, dataType, dataSubType)
 imgDir      ='%s/Images/%s/%s/' %(data_dir, dataType, dataSubType)
+
 resultType  ='baseline'
 fileTypes   = ['results', 'accuracy', 'evalQA', 'evalQuesType', 'evalAnsType']
 
 # An example result json file has been provided in './Results' folder.
 
 [resFile, accuracyFile, evalQAFile, evalQuesTypeFile, evalAnsTypeFile] = [
-    '%s/Results/%s_%s_%s_%s_%s.json'%(experiment_dir, taskType, dataType,
+    '%s/%s_%s_%s_%s_%s.json'%(experiment_dir, taskType, dataType,
                                       dataSubType, resultType, fileType) for fileType in fileTypes]
-figFile = '%s/Results/accbars.png' % experiment_dir
+figFile = '{0}/accuracy-bars-{1}'.format(experiment_dir, dataSubType)
 
 # create vqa object and vqaRes object
 vqa = VQA(annFile, quesFile)
@@ -67,6 +71,7 @@ print "Per Answer Type Accuracy is the following:"
 for ansType in vqaEval.accuracy['perAnswerType']:
 	print "%s : %.02f" %(ansType, vqaEval.accuracy['perAnswerType'][ansType])
 print "\n"
+
 # demo how to use evalQA to retrieve low score result
 evals = [quesId for quesId in vqaEval.evalQA if vqaEval.evalQA[quesId]<35]   #35 is per question percentage accuracy
 if len(evals) > 0:
@@ -91,14 +96,17 @@ if len(evals) > 0:
 # plot accuracy for various question types
 fig = plt.figure()
 # TODO(andrei): Rotate bar labels.
-# ax = fig.gca()
 plt.bar(range(len(vqaEval.accuracy['perQuestionType'])), vqaEval.accuracy[
     'perQuestionType'].values(), align='center')
 plt.xticks(range(len(vqaEval.accuracy['perQuestionType'])), vqaEval.accuracy['perQuestionType'].keys(), rotation='0',fontsize=10)
 plt.title('Per Question Type Accuracy', fontsize=10)
 plt.xlabel('Question Types', fontsize=10)
 plt.ylabel('Accuracy', fontsize=10)
-fig.savefig(figFile)
+
+# Save the figure in both raster and vector format. The latter looks very
+# pretty when included in LaTeX!
+fig.savefig(figFile + '.png')
+fig.savefig(figFile + '.eps')
 
 # save evaluation results to ./Results folder
 json.dump(vqaEval.accuracy,     open(accuracyFile,     'w'))
